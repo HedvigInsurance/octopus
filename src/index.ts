3c8a65a479b1e0d3420083709abaf44b69bcb965
Robin Andeer <robin.andeer@gmail.com>
2022-06-13 15:38:36 +0200

    Add Apollo Gateway setup
    Add example graphql subgraphs.
    Add TS setup.

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { ApolloServer } from "apollo-server";
+import { ApolloGateway } from "@apollo/gateway";
+
+require('dotenv').config()
+
+const gateway = new ApolloGateway();
+
+const server = new ApolloServer({
+  gateway,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Gateway ready at ${url}`);
+}).catch(err => {console.error(err)});
